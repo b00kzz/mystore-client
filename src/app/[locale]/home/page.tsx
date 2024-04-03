54c8e27a5d4368af1a31f73f76acd06637465adb
OASISAsx <wave001133@gmail.com>
2024-04-03 23:51:05 +0700

    add carousel

--- src/app/[locale]/home/page.tsx
+++ src/app/[locale]/home/page.tsx
@@ -1,42 +1,19 @@
-"use client"
+"use client";
 import React from "react";
 import CardMenu from "@/components/card-menu";
 import { useTranslations } from "next-intl";
-
+import CarouselHome from "@/components/CarouselHome";
 
 type Props = {};
 
-
-export default function HomeProduct({ }: Props) {
+export default function HomeProduct({}: Props) {
   const t = useTranslations("Menu");
   return (
     <>
-      {/* <div className="p-10 font-bold ">
-        <p>MENU</p>
-        <div className="flex justify-center items-center bg-black/30 w-full h-60 md:size-72 rounded-sm">
-          <div className="grid grid-cols-3 gap-4">
-            <button className="menu-home">
-              <p className="p-5  font-bold">TEST</p>
-            </button>
-            <button className="menu-home">
-              <p className="p-5  font-bold">TEST</p>
-            </button>
-            <button className="menu-home">
-              <p className="p-5  font-bold">TEST</p>
-            </button>
-            <button className="menu-home">
-              <p className="p-5  font-bold">TEST</p>
-            </button>
-            <button className="menu-home">
-              <p className="p-5  font-bold">TEST</p>
-            </button>
-            <button className="menu-home">
-              <p className="p-5  font-bold">TEST</p>
-            </button>
-          </div>
-        </div>
-      </div> */}
       <div>
+        <div className="flex justify-center items-center">
+          <CarouselHome />
+        </div>
         <p className="font-bold p-6">MENU</p>
         <CardMenu />
       </div>
